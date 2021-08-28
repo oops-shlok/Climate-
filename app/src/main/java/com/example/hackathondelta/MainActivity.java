6b4fc65f17204e9a6b66c246ed02571f4673cef5
oops-shlok <83498152+oops-shlok@users.noreply.github.com>
2021-08-28 13:10:52 +0530

    got current location

--- app/src/main/java/com/example/hackathondelta/MainActivity.java
+++ app/src/main/java/com/example/hackathondelta/MainActivity.java
@@ -1,14 +1,98 @@
 package com.example.hackathondelta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView current;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        current = (TextView) findViewById(R.id.current);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1000);
+        } else {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+            try {
+                String city = hereloc(location.getLatitude(), location.getLongitude());
+                current.setText(city);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Not Found", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1000: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+                    try {
+                        String city = hereloc(location.getLatitude(), location.getLongitude());
+                        current.setText(city);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this,"Not Found",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
+    private String hereloc(double lat, double lon){
+        String cityname = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address>addresses;
+        try{
+            addresses=geocoder.getFromLocation(lat,lon,10);
+            if(addresses.size()>0){
+                for(Address adr:addresses){
+                    if(adr.getLocality()!=null&& adr.getLocality().length()>0){
+                        cityname = adr.getLocality();
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cityname;
+    }
+
+
 }
